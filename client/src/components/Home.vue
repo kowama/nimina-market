5efb2acad72c5fcb698a0d712103fc519c59d818
ryuzaki-8 <ryuzaki.dev8@gmail.com>
2018-09-08 17:41:17 +0100

    setup the home page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,54 @@
 <template>
-    <div class="container-fluid">
+    <section id="main" class="container-fluid">
         <h1>Home page</h1>
-    </div>
+        <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselIndicators" data-slide-to="5"></li>
+
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="./../assets/img/image1.jpg" alt="1rst slide">
+                </div>
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="./../assets/img/image2.jpg" alt="2nd slide">
+                </div>
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="./../assets/img/image3.jpg" alt="3rd slide">
+                </div>
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="./../assets/img/image4.jpg" alt="4th slide">
+                </div>
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="./../assets/img/image5.jpg" alt="5th slide">
+                </div>
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="./../assets/img/image6.jpg" alt="6th slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon " aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
 
 </template>
 
 <script>
     
 </script>
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+@import '~styles/variables';
 
 </style>
 
