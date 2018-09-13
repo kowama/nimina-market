f11c107212c02e9805573c5a18c41c06b8e7a9b4
ryuzaki-8 <ryuzaki.dev8@gmail.com>
2018-09-13 19:34:36 +0100

    update user data Api is OK

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,6 +7,37 @@ router.post('/profile', authToken, (req, res) => {
 		user: req.user.getProfile()
 	});
 });
+router.post('/update', authToken, async (req, res) => {
+	try {
+		req.user.set({
+			name: req.body.name || req.user.name,
+			picture: req.body.picture || req.user.picture,
+			email: req.body.email || req.user.email,
+			isSeller: req.body.isSeller || req.user.isSeller,
+			address: req.body.address || req.user.address
+		});
+		let passwordUpdated = false;
+		if (req.body.updatePassword && req.body.password && req.body.password === req.body.passwordConfirm) {
+			req.user.set({
+				password: req.body.password
+			});
+			passwordUpdated = true;
+		}
+
+		const updatedUser = await req.user.save();
+		if (!updatedUser) {
+			return Promise.reject('unable to update user data');
+		}
+		res.json({
+			updatedUser: updatedUser.getProfile(),
+			passwordUpdated
+		});
+	} catch (err) {
+		res.status(422).json({
+			message: err.errmsg || err.message || 'unable to update user data'
+		});
+	}
+});
 router.delete('/logout', authToken, (req, res) => {
 	req.user
 		.removeToken(req.token)
